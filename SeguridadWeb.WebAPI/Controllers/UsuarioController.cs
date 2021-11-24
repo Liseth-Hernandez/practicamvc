b0e9f8d116beb17fe46f52cd3dd4393fa42826a2
Liseth Hernandez <Liseth1.Liseth2@outlook.com>
2021-11-24 01:46:44 -0600

    subiendo el proyecto

---
+++ SeguridadWeb.WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SeguridadWeb.EntidadesDeNegocio;
+using SeguridadWeb.LogicaDeNegocio;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SeguridadWeb.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private UsuarioBL usuarioBL = new UsuarioBL();
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public async Task<IEnumerable<Usuario>> Get()
+        {
+            return await usuarioBL.ObtenerTodosAsync();
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}")]
+        public async Task<Usuario> Get(int id)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = id;
+            return await usuarioBL.ObtenerPorIdAsync(usuario);
+        }
+
+        // POST api/<UsuarioController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Usuario usuario)
+        {
+            try
+            {
+                await usuarioBL.CrearAsync(usuario);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        // PUT api/<UsuarioController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Usuario usuario)
+        {
+
+            if (usuario.Id == id)
+            {
+                await usuarioBL.ModificarAsync(usuario);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Usuario usuario = new Usuario();
+                usuario.Id = id;
+                await usuarioBL.EliminarAsync(usuario);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Usuario>> Buscar([FromBody] object pUsuario)
+        {
+
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strUsuario = JsonSerializer.Serialize(pUsuario);
+            Usuario usuario = JsonSerializer.Deserialize<Usuario>(strUsuario, option);
+            return await usuarioBL.BuscarAsync(usuario);
+
+        }
+    }
+}
